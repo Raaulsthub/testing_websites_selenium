440eed6165a38ac942deaf50f1b5973b385f1125
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-06-03 10:55:30 -0300

    test of one button

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ import time
 import warnings
 import unittest
 
-warnings.filterwarnings("ignore", category=DeprecationWarning) # Ignore DeprecationWarning
 
 class WebPageTester(unittest.TestCase):
-    #constructor 
-    def __init__(self):
-        super().__init__()
+
+    def test_run_tests(self):
+        warnings.filterwarnings("ignore", category=DeprecationWarning) # Ignore DeprecationWarning
+        print('\n\n\t\t\tTESTING WEBPAGE', end='\n\n')
+        self.load_driver()
+        print('\n')
+        self.load_page('https://www.ucsd.edu/')
+        print('\n')
+        self.print_page_info()
+        print('\n')
+        self.buttons()
+        self.driver.quit()
 
     def load_driver(self):
         print('\tLoading driver')
@@ -37,48 +45,48 @@ class WebPageTester(unittest.TestCase):
         print("\tWebpage URL: " + self.driver.current_url)
         # print page title
         print("\tWebpage title: " + self.driver.title)
-        # print page source
-        # print(driver.page_source)
         return
 
-    def test_buttons(self):
+    def buttons(self):
         print("\t\t\tTESTING BUTTONS", end='\n\n')
 
         # BUTTON NUMBER ONE
-        print("\tButton number one", end='\n')
+        print("\t\t\tButton number one", end='\n')
         # getting button
         try:
             button = self.driver.find_element(By.XPATH, "//a[contains(@class, 'btn-default') and contains(@class, 'rt-btn-yellow')]")
-            # printing button text
-            print("\tButton text: " + button.text)
+
             # testing button name
+            print("\tButton text: " + button.text)
             expected_text = 'LEARN MORE ABOUT COMMENCEMENT'
+            self.assertEqual(button.text, expected_text)
+            print('\tText is working properly')
 
+            # testing button link
+            expected_link = 'https://commencement.ucsd.edu/'
+            self.assertEqual(button.get_attribute('href'), expected_link)
+            print('\tLink is working properly')
+
+            # tesing click
+            button.click()
+            new_link = self.driver.current_url # getting new link
+            expected_new_link = 'https://commencement.ucsd.edu/'
+            new_link = new_link.split('?')[0]
+            self.assertEqual(new_link, expected_new_link)
+            print('\tButton clicked successfully')
+
+            print('\tFound no errors on button 1')
         except:
-            print("\tError getting button")
+            print("\tError on button 1")
 
-    
-    def run_tests(self):
-        print('\n\n\t\t\tTESTING WEBPAGE', end='\n\n')
-        self.load_driver()
-        print('\n')
-        self.load_page('https://www.ucsd.edu/')
-        print('\n')
-        self.print_page_info()
-        print('\n')
-        self.test_buttons()
+
     
 
 def main():
-    tester = WebPageTester()
-    tester.run_tests()
-    
-
+    # tester = WebPageTester()
+    # tester.run_tests()
+    unittest.main()
     return
-
-
-
-
 
 if __name__ == "__main__":
     main()
