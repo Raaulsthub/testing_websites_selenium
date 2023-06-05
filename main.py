2acb95f04b719979cf8259816c340d1c9c78bb5e
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-06-04 23:05:38 -0300

    testing video, search box

--- main.py
+++ main.py
@@ -21,6 +21,11 @@ class WebPageTester(unittest.TestCase):
         self.print_page_info()
         print('\n')
         self.buttons()
+        print('\n')
+        self.video()
+        print('\n')
+        self.search_box()
+        time.sleep(15)
         self.driver.quit()
 
     def load_driver(self):
@@ -51,7 +56,7 @@ class WebPageTester(unittest.TestCase):
         print("\t\t\tTESTING BUTTONS", end='\n\n')
 
         # BUTTON NUMBER ONE
-        print("\t\t\tButton number one", end='\n')
+        print("\t\tButton number one", end='\n')
         # getting button
         try:
             button = self.driver.find_element(By.XPATH, "//a[contains(@class, 'btn-default') and contains(@class, 'rt-btn-yellow')]")
@@ -75,10 +80,137 @@ class WebPageTester(unittest.TestCase):
             self.assertEqual(new_link, expected_new_link)
             print('\tButton clicked successfully')
 
+            # testing new link
+            self.driver.get(new_link)
+            self.assertEqual(self.driver.current_url, expected_new_link)
+            print('\tNew link is working properly')
+
             print('\tFound no errors on button 1')
         except:
             print("\tError on button 1")
 
+
+        # Button number two
+        print("\n\t\tButton number two", end='\n')
+        # getting button
+        try:
+            button = self.driver.find_element(By.XPATH, '//*[@id="navbar"]/ul[1]/li[1]/a')
+            # testing button name
+            print("\tButton text: " + button.text)
+            expected_text = 'Students'
+            self.assertEqual(button.text, expected_text)
+            print('\tText is working properly')
+            # testing button link
+            expected_link = 'https://commencement.ucsd.edu/students/index.html'
+            self.assertEqual(button.get_attribute('href'), expected_link)
+            print('\tLink is working properly')
+            # tesing click
+            button.click()
+            new_link = self.driver.current_url # getting new link
+            expected_new_link = 'https://commencement.ucsd.edu/students/index.html'
+            new_link = new_link.split('?')[0]
+            self.assertEqual(new_link, expected_new_link)
+            print('\tButton clicked successfully')
+            # testing new link
+            self.driver.get(new_link)
+            self.assertEqual(self.driver.current_url, expected_new_link)
+            print('\tNew link is working properly')
+            print('\tFound no errors on button 2')
+        except:
+            print("\tError on button 2")
+
+    def video(self):
+        
+        try:
+            self.driver.get('https://ucsd.edu/')
+            # testing video
+            print("\t\t\tTESTING VIDEO", end='\n\n')
+            video_button = self.driver.find_element(By.XPATH, '//*[@id="a-main"]/section[1]/div/div/div[1]/p[2]/a')
+            print('\tFound video button')
+            video_button.click()
+            print('\tClicked video button')
+            current_url = self.driver.current_url
+            expected_url = 'https://www.youtube.com/watch?v=hmj_mp0bQ8s&feature=youtu.be'
+            self.assertEqual(current_url, expected_url)
+            print('\tGot to youtube video')
+            print('\tVideo is working properly')
+        except:
+            print("\tError on video")
+
+    def search_box(self):
+        print("\t\t\tTESTING SEARCH BOX", end='\n\n')
+        print("\t\t Testing blink mode")
+        try:
+            self.driver.get('https://students.ucsd.edu/student-life/events/')
+            # maximize window
+            self.driver.maximize_window()
+            # getting search box
+            search_box = self.driver.find_element(By.XPATH, '//*[@id="q"]')
+            print('\tFound search box')
+            # testing search box
+            search_box.send_keys('test')
+            print('\tSent keys to search box')
+            search_box.send_keys(Keys.RETURN)
+            print('\tPressed enter')
+
+            # clicking on one of the results
+            link = self.driver.find_element(By.XPATH, '//*[@id="___gcse_0"]/div/div/div/div[5]/div[2]/div/div/div[1]/div[1]/div[1]/div[1]/div/a')
+            print('\tFound search result')
+            link.click()
+            print('\tClicked on search result')
+            print('\tPage name: ' + self.driver.title)
+            # checking new link
+            current_url = self.driver.current_url
+            expected_url = 'https://blink.ucsd.edu/safety/resources/public-health/covid-19/index.html'
+            self.assertEqual(current_url, expected_url)
+            print('\tSearch box (blink mode) is working properly')
+        except:
+            print("\tError on search box (blink mode)")
+
+        print("\n\t\t Testing Faculty and Staff mode")
+        try:
+            self.driver.get('https://students.ucsd.edu/student-life/events/')
+            # maximize window
+            self.driver.maximize_window()
+            # activating faculty and staff mode
+            button = self.driver.find_element(By.XPATH, '//*[@id="cse-search-box"]/label[2]')
+            button.click()
+            print('\tSwitched to faculty and staff mode')
+            # getting search box
+            search_box = self.driver.find_element(By.XPATH, '//*[@id="entry"]')
+            print('\tFound search box')
+            # testing search box
+            search_box.send_keys('computer science')
+            print('\tSent keys to search box')
+            search_box.send_keys(Keys.RETURN)
+            print('\tPressed enter')
+            # checking new link
+            current_url = self.driver.current_url
+            expected_url = 'https://act.ucsd.edu/directory/search?search-scope=faculty-staff&entry=computer+science'
+            self.assertEqual(current_url, expected_url)
+            print('\tSearch box (faculty and staff mode) is working properly')
+        except:
+            print("\tError on search box (faculty and staff mode)")
+
+
+
+        
+
+
+        
+
+        
+
+
+
+
+
+            
+
+
+
+
+        
 
     
 
